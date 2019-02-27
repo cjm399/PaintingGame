f1e48d3016bb88ab7c7b8e6003ef3d2d0344bbfb
Chris McCole <chrismccole1@gmail.com>
2019-02-26 23:48:51 -0500

    Touchups to the painting scripts as well as some extra things being kept track of in the GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     [HideInInspector]
     public int screenHeight;
     [HideInInspector]
     public int screenWidth;
+    public int sizeMultiplyer;
+    public Color currentPaintColor;
+    public GameObject currentPaintCanvas;
+    public float percentComplete;
+    public int totalPixelCount;
+    public int currPixelCount;
+    public Texture2D currentTexture;
     [HideInInspector]
     public static GameManager Instance;
 
@@ -26,12 +33,46 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        currentPaintColor = Color.red;
+        sizeMultiplyer = 5;
+        currPixelCount = 0;
+        totalPixelCount = 0;
+        currentTexture = currentPaintCanvas.GetComponent<Image>().sprite.texture;
+        Color[] colors = currentTexture.GetPixels();
+        for(int i = 0; i < colors.Length; i++)
+        {
+            if(colors[i].a != 0 && !IsBasicallyBlack(colors[i]))
+            {
+                totalPixelCount++;
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         updateResolution();
+        if(currPixelCount != 0)
+        {
+            percentComplete = (float)currPixelCount / (float)totalPixelCount;
+        }
+        
+    }
+
+    public bool IsBasicallyBlack(Color color, float epsilon = .039f)
+    {
+        if (color.r >= 0 && color.r <= epsilon)
+        {
+            if (color.g >= 0 && color.g <= epsilon)
+            {
+                if (color.b >= 0 && color.b <= epsilon)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     private void updateResolution()
@@ -39,4 +80,5 @@ public class GameManager : MonoBehaviour
         screenHeight = Screen.height;
         screenWidth = Screen.width;
     }
+
 }
--- Assets/Scripts/Painting.cs
+++ Assets/Scripts/Painting.cs
@@ -11,14 +11,15 @@ public class Painting : MonoBehaviour
     Sprite paintingSprite;
     int screenWidth;
     int screenHeight;
+    Texture2D originalTexture;
 
     void Start()
     {
         paintingImage = paintingCanvas.GetComponent<Image>();
         paintingSprite = paintingImage.sprite;
         texture = paintingSprite.texture;
-        Color[] colors = texture.GetPixels();
-        Color white = Color.white;
+        originalTexture = new Texture2D(texture.width, texture.height);
+        originalTexture.SetPixels(texture.GetPixels());
         texture.Apply();
 
     }
@@ -33,6 +34,9 @@ public class Painting : MonoBehaviour
         Texture2D old = new Texture2D((int)texture.width, (int)texture.height);
         Color[] colors = texture.GetPixels(0, 0, (int)texture.width, (int)texture.height);
         old.SetPixels(colors);
+        Color currentPaintColor = GameManager.Instance.currentPaintColor;
+        int sizeMultiplyer = GameManager.Instance.sizeMultiplyer;
+        int count = 0;
 
         if (Input.GetKey(KeyCode.Mouse0))
         {
@@ -54,10 +58,10 @@ public class Painting : MonoBehaviour
             Vector3 pixel = position - FirstPixel;
             pixel.x = pixel.x * texture.width / paintingRectTransform.sizeDelta.x;
             pixel.y = (paintingRectTransform.sizeDelta.y + pixel.y) * texture.height / paintingRectTransform.sizeDelta.y;
-            Debug.Log(pixel);
+
             pixel = new Vector3(pixel.x, pixel.y, pixel.z);
 
-            int squareWidth = (int)Mathf.Pow(2, 6);
+            int squareWidth = 2 * sizeMultiplyer;
 
             if (pixel.x > 0 && pixel.y > 0 && pixel.x < texture.width && pixel.y < texture.height)
             {
@@ -66,15 +70,35 @@ public class Painting : MonoBehaviour
                     for (int j = 0; j < squareWidth; j++)
                     {
                         Color color = old.GetPixel((int)pixel.x - (squareWidth / 2) + i, (int)pixel.y - (squareWidth / 2) + j);
-                        if (color.a != 0 && (color.r != 0 && color.g != 0 && color.b != 0))
-                            old.SetPixel((int)pixel.x - (squareWidth / 2) + i, (int)pixel.y - (squareWidth / 2) + j, Color.red);
+                        if (color.a != 0 && !IsBasicallyBlack(color))
+                        {
+                            old.SetPixel((int)pixel.x - (squareWidth / 2) + i, (int)pixel.y - (squareWidth / 2) + j, currentPaintColor);
+                            if(color == originalTexture.GetPixel((int)pixel.x - (squareWidth / 2) + i, (int)pixel.y - (squareWidth / 2) + j))
+                                count++;
+                        }
                     }
                 }
             }
         }
+        GameManager.Instance.currPixelCount += count;
 
         texture.SetPixels(old.GetPixels(0,0, old.width, old.height));
         texture.Apply();
+    }
+
+    public bool IsBasicallyBlack(Color color, float epsilon = .039f)
+    {
+        if (color.r >= 0 && color.r <= epsilon)
+        {
+            if (color.g >= 0 && color.g <= epsilon)
+            {
+                if (color.b >= 0 && color.b <= epsilon)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }
