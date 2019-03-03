672c679b6d99f6cfea2deaa3138ad089f67e4cfc
Chris McCole <chrismccole1@gmail.com>
2019-03-03 18:44:46 -0500

    Resizing changes to the UI and text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 public class GameManager : MonoBehaviour
 {
     public enum State {
@@ -13,6 +15,12 @@ public class GameManager : MonoBehaviour
     public int screenHeight;
     [HideInInspector]
     public int screenWidth;
+    [HideInInspector]
+    public int oldScreenHeight = 0;
+    [HideInInspector]
+    public Button finishPictureButton;
+    [HideInInspector]
+    public int oldScreenWidth = 0;
     public int sizeMultiplyer;
     public Color currentPaintColor;
     public GameObject currentPaintCanvas;
@@ -38,11 +46,11 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this);
         }
-        updateResolution();
     }
 
     void Start()
     {
+        finishPictureButton = GameObject.Find("FinishPicture").GetComponent<Button>();
         paintCanvasIndex = 0;
         currentPaintColor = Color.red;
         sizeMultiplyer = 5;
@@ -50,6 +58,8 @@ public class GameManager : MonoBehaviour
         SetPaintCounters();
         currentState = State.PAINTING;
 
+
+        updateResolution();
     }
 
     // Update is called once per frame
@@ -63,26 +73,35 @@ public class GameManager : MonoBehaviour
 
         if (percentComplete >= .9f)
         {
-            if(paintCanvasIndex == PaintCanvases.Length-1)
+            finishPictureButton.interactable = true;
+        }
+        else
+        {
+            finishPictureButton.interactable = false;
+        }
+    }
+
+    public void NextPicture()
+    {
+        if (paintCanvasIndex == PaintCanvases.Length - 1)
+        {
+            currentState = State.FINISHED;
+            currentPaintCanvas.GetComponent<Painting>().enabled = false;
+            for (int i = 0; i < PaintCanvases.Length; i++)
             {
-                currentState = State.FINISHED;
-                currentPaintCanvas.GetComponent<Painting>().enabled = false;
-                for(int i = 0; i < PaintCanvases.Length; i++)
-                {
-                    PaintCanvasPair[i].SetActive(false);
-                    PaintCanvases[i].SetActive(true);
-                }
+                PaintCanvasPair[i].SetActive(false);
+                PaintCanvases[i].SetActive(true);
             }
-            else
-            {
-                currentPaintCanvas.GetComponent<Painting>().enabled = false;
-                currentPaintCanvas.SetActive(false);
-                PaintCanvasPair[paintCanvasIndex].SetActive(false);
-                currentPaintCanvas = PaintCanvases[++paintCanvasIndex];
-                PaintCanvasPair[paintCanvasIndex].SetActive(true);
-                currentPaintCanvas.SetActive(true);
-                SetPaintCounters();
-            }
+        }
+        else
+        {
+            currentPaintCanvas.GetComponent<Painting>().enabled = false;
+            currentPaintCanvas.SetActive(false);
+            PaintCanvasPair[paintCanvasIndex].SetActive(false);
+            currentPaintCanvas = PaintCanvases[++paintCanvasIndex];
+            PaintCanvasPair[paintCanvasIndex].SetActive(true);
+            currentPaintCanvas.SetActive(true);
+            SetPaintCounters();
         }
     }
 
@@ -105,6 +124,13 @@ public class GameManager : MonoBehaviour
     {
         screenHeight = Screen.height;
         screenWidth = Screen.width;
+
+        if(screenHeight != oldScreenHeight || screenWidth != oldScreenWidth)
+        {
+            SetPaintingResolutions();
+        }
+        oldScreenHeight = screenHeight;
+        oldScreenWidth = screenWidth;
     }
 
     private void SetPaintCounters()
@@ -121,6 +147,40 @@ public class GameManager : MonoBehaviour
                 totalPixelCount++;
             }
         }
+    }
+
+    private void SetPaintingResolutions()
+    {
+        Vector2 resolution = new Vector2(screenWidth, screenHeight);
+        for (int i = 0; i < PaintCanvases.Length; i++)
+        {
+            PaintCanvases[i].GetComponent<RectTransform>().sizeDelta = resolution;
+            PaintCanvasPair[i].GetComponent<RectTransform>().sizeDelta = resolution;
+        }
+
+        RectTransform FadeGraphic = GameObject.Find("FadeGraphic").GetComponent<RectTransform>();
+        RectTransform ToolBar = GameObject.Find("ToolBar").GetComponent<RectTransform>();
+        RectTransform DyeDropper = GameObject.Find("DyeDropper").GetComponent<RectTransform>();
+        RectTransform ColorPicker = GameObject.Find("ColorPicker").GetComponent<RectTransform>();
+        RectTransform Slider = GameObject.Find("Slider").GetComponent<RectTransform>();
+        RectTransform finishPic = finishPictureButton.GetComponent<RectTransform>();
+
+        FadeGraphic.sizeDelta = resolution;
+        ColorPicker.sizeDelta = resolution * .75f;
+        ToolBar.sizeDelta = new Vector2(screenWidth, screenHeight / 10);
+        ToolBar.localPosition = new Vector3(0, (screenHeight - ToolBar.sizeDelta.y) / 2, 0);
+
+        DyeDropper.sizeDelta = new Vector2(screenHeight / 10, screenHeight / 10);
+        DyeDropper.localPosition = new Vector3((screenWidth-DyeDropper.sizeDelta.x)/2, ToolBar.localPosition.y, 0);
+
+        float sliderWidthDefault = 200f / 1920f;
+        float sliderHeightDefault = 20f / 1080f;
+        Slider.sizeDelta = new Vector2((float)screenWidth* sliderWidthDefault, (float)screenHeight* sliderHeightDefault);
+        Slider.localPosition = new Vector3(((screenWidth - DyeDropper.sizeDelta.x) / 2)-Slider.sizeDelta.x - DyeDropper.sizeDelta.x, ToolBar.localPosition.y, 0);
+
+        finishPic.sizeDelta = new Vector2(screenWidth / 10, (screenHeight / 10)/4);
+        finishPic.localPosition = new Vector3(((-screenWidth+finishPic.sizeDelta.x)/2)+finishPic.sizeDelta.x, ToolBar.localPosition.y, 0);
+        finishPic.GetChild(0).GetComponent<TextMeshProUGUI>().fontSize = screenWidth * .0125f;
     }
 
 }
