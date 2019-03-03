4556f8c46a65ab2c28090df2a8dee433e1c91959
Chris McCole <chrismccole1@gmail.com>
2019-03-03 12:21:37 -0500

    Optimization code

--- Assets/Scripts/Painting.cs
+++ Assets/Scripts/Painting.cs
@@ -13,7 +13,6 @@ public class Painting : MonoBehaviour
     int screenHeight;
     Texture2D originalTexture;
     Dictionary<Vector2, bool> myBlackPixels;
-    Texture2D old;
 
     void Start()
     {
@@ -25,24 +24,22 @@ public class Painting : MonoBehaviour
         originalTexture = new Texture2D(texture.width, texture.height);
         originalTexture.SetPixels(texture.GetPixels());
         texture.Apply();
-        old = new Texture2D((int)texture.width, (int)texture.height);
     }
 
     // Update is called once per frame
     void Update()
     {
-        screenHeight = GameManager.Instance.screenHeight;
-        screenWidth = GameManager.Instance.screenWidth;
-        RectTransform paintingRectTransform = paintingCanvas.GetComponent<RectTransform>();
-        Vector3 position;
-        Color[] colors = texture.GetPixels(0, 0, (int)texture.width, (int)texture.height);
-        old.SetPixels(colors);
-        Color currentPaintColor = GameManager.Instance.currentPaintColor;
-        int sizeMultiplyer = GameManager.Instance.sizeMultiplyer;
         int count = 0;
 
         if (Input.GetKey(KeyCode.Mouse0) && GameManager.Instance.currentState == GameManager.State.PAINTING)
         {
+            screenHeight = GameManager.Instance.screenHeight;
+            screenWidth = GameManager.Instance.screenWidth;
+            RectTransform paintingRectTransform = paintingCanvas.GetComponent<RectTransform>();
+            Vector3 position;
+
+            int sizeMultiplyer = GameManager.Instance.sizeMultiplyer;
+
             position = Camera.main.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, paintingCanvas.transform.position.z));
             position.x *= screenWidth;
             position.y *= screenHeight;
@@ -68,11 +65,12 @@ public class Painting : MonoBehaviour
 
             if (pixel.x > 0 && pixel.y > 0 && pixel.x < texture.width && pixel.y < texture.height)
             {
+                Color currentPaintColor = GameManager.Instance.currentPaintColor;
                 for (int i = 0; i < squareWidth; i++)
                 {
                     for (int j = 0; j < squareWidth; j++)
                     {
-                        Color color = old.GetPixel((int)pixel.x - (squareWidth / 2) + i, (int)pixel.y - (squareWidth / 2) + j);
+                        Color color = texture.GetPixel((int)pixel.x - (squareWidth / 2) + i, (int)pixel.y - (squareWidth / 2) + j);
                         Vector2 vec = new Vector2((int)pixel.x - (squareWidth / 2) + i, (int)pixel.y - (squareWidth / 2) + j);
                         if (color.a != 0 && (!IsBasicallyBlack(color) || myBlackPixels.ContainsKey(vec)))
                         {
@@ -85,18 +83,16 @@ public class Painting : MonoBehaviour
                                 }
 
                             }
-                            old.SetPixel((int)pixel.x - (squareWidth / 2) + i, (int)pixel.y - (squareWidth / 2) + j, currentPaintColor);
+                            texture.SetPixel((int)pixel.x - (squareWidth / 2) + i, (int)pixel.y - (squareWidth / 2) + j, currentPaintColor);
                             if(color == originalTexture.GetPixel((int)pixel.x - (squareWidth / 2) + i, (int)pixel.y - (squareWidth / 2) + j))
                                 count++;
                         }
                     }
                 }
-            }
+                texture.Apply();
+                GameManager.Instance.currPixelCount += count;
+            }  
         }
-        GameManager.Instance.currPixelCount += count;
-
-        texture.SetPixels(old.GetPixels(0,0, old.width, old.height));
-        texture.Apply();
     }
 
     public bool IsBasicallyBlack(Color color, float epsilon = .35f)
